e31fe97c4f8ba0f91e8d844ed3dcf1c72c93a73c
dgaid <dgaid@softserveinc.com>
2019-02-28 17:39:05 +0200

    class based view

--- website/app.py
+++ website/app.py
@@ -1,12 +1,17 @@
 import flask
+from flask import views
+
+
+class IndexView(views.MethodView):
+
+    def get(self):
+        return flask.render_template('index.html')
 
 
 def create_app() -> flask.Flask:
     app = flask.Flask(__name__)
 
-    @app.route('/')
-    def _index():
-        return flask.render_template('base.html')
+    app.add_url_rule('/', view_func=IndexView.as_view('index'))
 
     return app
 
